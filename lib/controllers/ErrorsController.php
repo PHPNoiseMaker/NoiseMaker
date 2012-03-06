a6c342890f4fa0d5f60ae907ce57dfc9bfff886f
Justin Kaufman <jkaufman@lodgeistics.net>
2012-03-06 00:08:08 -0500

    passing vars to exception rendered pages
    Signed-off-by: Justin Kaufman <jkaufman@lodgeistics.net>

--- lib/controllers/ErrorsController.php
+++ lib/controllers/ErrorsController.php
@@ -2,8 +2,21 @@
 require_once('Controllers/AppController.php');
 class ErrorsController extends AppController {
 	
-	public function index($arg = null) {
+	public function index($params = null) {
 		$this->viewFile = 'error';
+		
+		if(is_array($params)) {
+			if(
+				isset($params['error']['message'])
+			) {
+				
+					$this->set('message', $params['error']['message']);
+				
+				
+			} else {
+				$this->set('message', '');
+			}
+		}
 	}
 
 }
