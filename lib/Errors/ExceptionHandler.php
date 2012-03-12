7805afc69b0d052f0ab1978d0ec69fcabf305062
Justin Kaufman <jkaufman@lodgeistics.net>
2012-03-11 22:47:32 -0400

    exception handler

--- lib/Errors/ExceptionHandler.php
+++ lib/Errors/ExceptionHandler.php
@@ -16,10 +16,18 @@ class ExceptionHandler {
 		App::uses('ErrorsController', 'Controller');
 		$controller = new ErrorsController(new Request(), new Response());
 		
+		$debugLevel = Config::getConfig('debug');
+		if($debugLevel > 0) {
+			$message = $e->getMessage();
+			$code = $e->getCode();
+		} else {
+			$message = 'Not Found';
+			$code = 404;
+		}
 		$params = array(
 			'error' => array(
-				'message' => $e->getMessage(),
-				'code' => $e->getCode()
+				'message' => $message,
+				'code' => $code
 			)
 		);
 		$controller->displayException($params);
@@ -42,14 +50,26 @@ class ExceptionHandler {
 		App::uses('ErrorsController', 'Controller');
 		$controller = new ErrorsController(new Request(), new Response());
 		
-		$params = array(
-			'error' => array(
-				'message' => $errorMsg,
-				'code' => $errorNumber,
-				'file' => $errorFile,
-				'line' => $errorLine
-			)
-		);
+		$debugLevel = Config::getConfig('debug');
+		if($debugLevel > 0) {
+			$params = array(
+				'error' => array(
+					'message' => $errorMsg,
+					'code' => $errorNumber,
+					'file' => $errorFile,
+					'line' => $errorLine
+				)
+			);
+		} else {
+			$params = array(
+				'error' => array(
+					'message' => 'Not Found',
+					'code' => 404
+				)
+			);
+
+		}
+		
 		$controller->displayError($params);
 		$controller->render('Errors');
 		//exit;
